d063155fa0898794af627e044ec0bcc0d7838b70
saloni chhaparwal <87599792+chhsaloni@users.noreply.github.com>
2026-02-08 21:43:36 +0530

    Time: 0 ms (100.00%) | Memory: 53.8 MB (78.95%) - LeetSync

--- 1764-maximum-repeating-substring/maximum-repeating-substring.js
+++ 1764-maximum-repeating-substring/maximum-repeating-substring.js
@@ -6,9 +6,8 @@
 var maxRepeating = function (sequence, word) {
     let str = word;
     let counter = 0;
-    if (!sequence.includes(word))
-        return 0
-        
+   
+
     while (sequence.includes(str)) {
         counter++;
         str += word;
